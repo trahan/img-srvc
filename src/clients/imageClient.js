d8a3092c66c15beaf58b0890fc2b3bc6d6994232
Matthew Trahan <matt.trahan@gmail.com>
2019-03-24 19:09:52 -0500

    Fix tabs

--- src/clients/imageClient.js
+++ src/clients/imageClient.js
@@ -17,30 +17,30 @@ const sizes = {
   the covers, so it can easily be extended for other operations.
 */
 class ImageClient {
-	constructor(s3, db) {
-		this._s3 = s3;
-		this._db = db;
-	}
+  constructor(s3, db) {
+    this._s3 = s3;
+    this._db = db;
+  }
 
-	saveImage(contentType, imageName, imageSize, imageData) {
-		return this._s3.uploadImage(contentType, imageData)
+  saveImage(contentType, imageName, imageSize, imageData) {
+    return this._s3.uploadImage(contentType, imageData)
       .then((key) => this._saveFileKey(contentType, imageName, imageSize, key))
       .then((result) => result[0])
-	}
+  }
 
   listImages() {
-  	return this._db.select('id', 'name', 'content_type', 'size').from('files')
+    return this._db.select('id', 'name', 'content_type', 'size').from('files')
   }
 
   loadAndResize(id, sizeName) {
-  	var state = {}
-  	return this._loadImageMetadata(id)
-  	  .then((metadata) => {
-  	    state.name = metadata.name;
-      	state.contentType = metadata.content_type;
-      	return this._s3.loadImage(metadata.s3_key);
-  	  })
-  	  .then((image) => this._resizeImage(state.contentType, sizeName, state.name, image));
+    var state = {}
+    return this._loadImageMetadata(id)
+      .then((metadata) => {
+        state.name = metadata.name;
+        state.contentType = metadata.content_type;
+        return this._s3.loadImage(metadata.s3_key);
+      })
+      .then((image) => this._resizeImage(state.contentType, sizeName, state.name, image));
   }
 
   _saveFileKey(contentType, imageName, imageSize, key) {
@@ -62,7 +62,7 @@ class ImageClient {
   }
 
   _resizeImage(contentType, sizeName, imageName, imageData) {
-  	if (sizeName == 'original') {
+    if (sizeName == 'original') {
       return {contentType: contentType, imageData: imageData}
     }
 
@@ -72,13 +72,13 @@ class ImageClient {
     }
 
     return new Promise((resolve, reject) => {
-    	gm(imageData, imageName)
-	      .resize(size[0],size[1])
-	      .toBuffer('JPEG', (err, buffer) => {
-	        if (err) reject(err);
-	        resolve({contentType: contentType, imageData: buffer});
-	      })
-	  });
+      gm(imageData, imageName)
+        .resize(size[0],size[1])
+        .toBuffer('JPEG', (err, buffer) => {
+          if (err) reject(err);
+          resolve({contentType: contentType, imageData: buffer});
+        })
+    });
   }
 }
 
